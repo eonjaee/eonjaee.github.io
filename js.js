41794a8aea5669fd006a19f9582c8b9439620051
eonjaee <102024936+eonjaee@users.noreply.github.com>
2022-04-03 05:41:31 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -42,23 +42,7 @@ try {
     }
   })
 } catch (e) {
-
 }
-//
-// Login/Register box
-//
-
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const loginbox = document.getElementById('loginbox');
-
-signUpButton.addEventListener('click', () => {
-  loginbox.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-  loginbox.classList.remove("right-panel-active");
-});
 
 function tbc() {
   alert("Feature not implemented yet.")
@@ -68,30 +52,11 @@ function tbc1() {
   alert("Coming soon!")
 }
 
-// Get the modal
-var modal = document.getElementById("loginBox");
-
-// Get the button that opens the modal
-var btn = document.getElementById("login");
-
-// Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "flex";
-}
-
-// When the user clicks on <span> (x), close the modal
-// span.onclick = function () {
-//   modal.style.display = "none";
-// }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+  // if (event.target == modal) {
+  //   modal.style.display = "none";
+  // }
   if (event.target == listOverlay) {
     listOverlay.style.display = "none";
     listOverlay1.style.display = "block";
